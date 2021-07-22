2a48a1277bd0f93273bac2016098901305d098a2
ProtectorYT364 <86392268+ProtectorYT364@users.noreply.github.com>
2021-07-22 09:09:09 +0530

    Ez pz fix

--- gomint-server/src/main/java/io/gomint/server/network/Protocol.java
+++ gomint-server/src/main/java/io/gomint/server/network/Protocol.java
@@ -59,8 +59,8 @@ public final class Protocol {
     // MC:PE Protocol ID
     public static final int MINECRAFT_PE_BETA_PROTOCOL_VERSION = -1;
     public static final int MINECRAFT_PE_NEXT_STABLE_PROTOCOL_VERSION = -1;
-    public static final int MINECRAFT_PE_PROTOCOL_VERSION = 422;
-    public static final String MINECRAFT_PE_NETWORK_VERSION = "1.16.200";
+    public static final int MINECRAFT_PE_PROTOCOL_VERSION = 448;
+    public static final String MINECRAFT_PE_NETWORK_VERSION = "1.17.10";
 
     // ========================================= PACKET IDS ========================================= //
     public static final byte BATCH_MAGIC = (byte) 0xfe;
